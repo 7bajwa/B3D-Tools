ef6020614b99ffc271bcf3caca31b76173e05f8b
Amanpreet Bajwa <amanpreetbajwa7@gmail.com>
2021-01-11 23:27:53 +0100

    Edited the top menu script

--- testop.py
+++ testop.py
@@ -3,7 +3,7 @@ import bpy
 class Flatten(bpy.types.Operator):
     bl_idname = "view3d.flat_x"
     bl_label = ""
-    bl_description = "Flatten the selection to a axis"
+    bl_description = "Flatten the selection to a certain axis"
 
     def execute(self, context):
         bpy.ops.transform.resize(0,1,1)
